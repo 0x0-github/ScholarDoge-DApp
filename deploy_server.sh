468cbd2021c66ae49dc9d9b9c064c19484c3e67e
Kahsyaj <orochiborus@gmail.com>
2021-10-04 18:36:18 +0200

    Updated app logos

--- deploy_server.sh
+++ deploy_server.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 cd ~/ScholarDoge-DApp/
+git stash
 git pull
-npm install
-npm run build
-sudo rm -r /var/www/html/scholar_doge_dapp/*
-sudo cp -r ~/ScholarDoge-DApp/build/* /var/www/html/scholar_doge_dapp/
+chmod +x deply_server.sh
+npm install && npm run build
+sudo rm -r /var/www/html/scholar_doge_dapp/* && echo "[DEPLOYMENT] : Removed old version"
+sudo cp -r ~/ScholarDoge-DApp/build/* /var/www/html/scholar_doge_dapp/ && echo "[DEPLOYMENT] : Added new version"
