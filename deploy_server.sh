8243721741f785eb5df8b0acfa3a2d7150090e27
Kahsyaj <orochiborus@gmail.com>
2021-10-04 18:37:34 +0200

    Updated deploy script

--- deploy_server.sh
+++ deploy_server.sh
@@ -3,7 +3,6 @@
 cd ~/ScholarDoge-DApp/
 git stash
 git pull
-chmod +x deply_server.sh
 npm install && npm run build
 sudo rm -r /var/www/html/scholar_doge_dapp/* && echo "[DEPLOYMENT] : Removed old version"
 sudo cp -r ~/ScholarDoge-DApp/build/* /var/www/html/scholar_doge_dapp/ && echo "[DEPLOYMENT] : Added new version"
